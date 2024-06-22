b046afc4974b3f1b9f58d76b695033829e4148ff
9a_orn <Lum.Gayoung@gmail.com>
2024-06-22 22:50:45 +0900

    Style: feat/rhythmList 작업 중

--- src/pages/MyRhythm.tsx
+++ src/pages/MyRhythm.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Weather from '../components/Weather';
 import Calendar from '../components/Calendar';
+import AddTodo from '../components/AddTodo';
 
 export default function MyRhythm() {
   return (
@@ -9,6 +10,7 @@ export default function MyRhythm() {
       <StyledHomeBox>
         <Weather />
         <Calendar />
+        <AddTodo />
       </StyledHomeBox>
     </StyledHomeWrapper>
   );
