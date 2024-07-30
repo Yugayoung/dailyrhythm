8f69cebc753f76c2d2b7c41a57c455311f31376d
9a_orn <Lum.Gayoung@gmail.com>
2024-07-30 18:57:11 +0900

    Style: 코드 구조 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -46,7 +46,6 @@ export default function Home() {
           </StyledArrowBox>
         </StyledHomeTopBox>
       </StyledHomeTopWrapper>
-
       <StyledHomeMiddleWrapper>
         <StyledHomeMiddleTextBox>
           <p>" </p>
